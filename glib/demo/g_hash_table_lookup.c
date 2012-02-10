3b17cccfd211c240ca611cbe4a7ef46132522091
Airead Fan <fgh1987168@gmail.com>
2012-02-10 12:06:59 +0800

    learn glib GHashTable insert string

--- glib/demo/g_hash_table_lookup.c
+++ glib/demo/g_hash_table_lookup.c
@@ -3,26 +3,39 @@
  * @date    2011 12月 19 15:46:17 CST
  ********************************************************/
 
-#if 0
-hash_table :
-a GHashTable.
+/**
+   hash_table :
+   a GHashTable.
 
-key :
-the key to look up.
+   key :
+   the key to look up.
 
-Returns :
-the associated value, or NULL if the key is not found.
-#endif
+   Returns :
+   the associated value, or NULL if the key is not found.
+*/
     
 #include <glib.h>
     
 
-    int look_up(GHashTable *table, gchar *key)
+int look_up_num(GHashTable *table, gchar *key)
 {
     gint *value;
 
     if ((value = g_hash_table_lookup(table, key))) {
         g_print("%s exist: %d\n", key, *value);
+    } else {
+        g_print("%s not exist\n", key);
+    }
+
+    return 0;
+}
+
+int look_up_str(GHashTable *table, gchar *key)
+{
+    gchar *str;
+
+    if ((str = g_hash_table_lookup(table, key))) {
+        g_print("%s exist: %s\n", key, str);
     } else {
         g_print("%s not exist\n", key);
     }
@@ -55,10 +68,14 @@ int main(int argc, char *argv[])
     g_hash_table_insert(table, "three", NULL);
     g_print("table size: %u\n", g_hash_table_size(table));
 
-    look_up(table, "one");
-    look_up(table, "two");
-    look_up(table, "three");
-    look_up(table, "four");
+    g_hash_table_insert(table, "four", "fourth");
+    g_print("table size: %u\n", g_hash_table_size(table));
+
+    look_up_num(table, "one");
+    look_up_num(table, "two");
+    look_up_num(table, "three");
+    look_up_str(table, "four");
+    look_up_num(table, "five");
 
     g_hash_table_destroy(table);
 
