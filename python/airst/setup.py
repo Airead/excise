4e05947a06fc69b25e4722ee9c55d7851e492c39
Airead Fan <fgh1987168@gmail.com>
2013-07-18 23:38:29 +0800

    add buildout config

---
+++ python/airst/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airst",
+    version="0.1",
+    description="My study module",
+    author="Airead Fan",
+    url="http://www.aireadfun.com",
+    license="GPL",
+    package_dir={'': 'src'}
+)
