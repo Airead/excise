eed020ce99ce2bc96f8707da00027105a3f23913
Airead Fan <fanrenhao@crazyhornets.com>
2014-06-03 17:49:34 +0800

    understand smartTable

--- angularjs/demo/table/table.js
+++ angularjs/demo/table/table.js
@@ -3,20 +3,20 @@
 
     var tableModule = angular.module('table', []);
 
+    tableModule.constant('DefaultTableConfiguration', {
+        selectionMode: 'none',
+        isGlobalSearchActivated: false,
+        displaySelectionCheckbox: false,
+        isPaginationEnabled: true,
+        itemsByPage: 10,
+        maxSize: 5,
+
+        //just to remind available option
+        sortAlgorithm: '',
+        filterAlgorithm: ''
+    });
+
     tableModule.directive('myTable', function () {
-        var config = {
-            selectionMode: 'none',
-            isGlobalSearchActivated: false,
-            displaySelectionCheckbox: false,
-            isPaginationEnabled: true,
-            itemsByPage: 10,
-            maxSize: 5,
-
-            //just to remind available option
-            sortAlgorithm: '',
-            filterAlgorithm: ''
-        };
-
         return {
             restrict: 'EA',
             scope: {
@@ -28,7 +28,14 @@
             templateUrl: 'smartTable.html',
             controller: 'TableCtrl',
             link: function (scope, element, attr, ctrl) {
-                console.log('ctlr.desc: ', ctrl.desc);
+                console.log('ctrl.desc: ', ctrl.desc);
+
+                var i;
+                scope.$watch('columnCollection', function () {
+                    for (i = 0; i < scope.columnCollection.length; i++) {
+                        ctrl.insertColumn(scope.columnCollection[i]);
+                    }
+                });
             }
         };
     });
@@ -37,6 +44,10 @@
         return {
             restrict: 'C',
             require: '^myTable',
+            templateUrl: 'globalSearchCell.html',
+            link: function (scope, element, attr, ctrl) {
+                scope.searchValue = 'default';
+            }
         }
     });
 
@@ -44,8 +55,26 @@
         '$scope',
         function ($scope) {
             $scope.title = 'my directive table';
+            $scope.columns = [];
 
             this.desc = 'this is TableCtrl';
+
+            this.insertColumn = function (columnConfig, index) {
+                var config = {
+                    'headerTemplateUrl': 'defaultHeader.html'
+                };
+                var column = angular.extend({}, columnConfig, config);
+
+                insertAt($scope.columns, index, column);
+            };
+
+            function insertAt(arrayRef, index, item) {
+                if (index >= 0 && index < arrayRef.length) {
+                    arrayRef.splice(index, 0, item);
+                } else {
+                    arrayRef.push(item);
+                }
+            }
         }
     ]);
 })(angular);
--- angularjs/demo/table/test.js
+++ angularjs/demo/table/test.js
@@ -6,7 +6,19 @@ app = angular.module('mainApp', ['table']);
 app.controller('TestCtl', [
     '$scope',
     function ($scope) {
-        $scope.title = 'Table'
+        $scope.title = 'Table';
+
+        $scope.data = [
+            {'Name': 'name1', 'age': 1, 'email': 'u1@a.com'},
+            {'Name': 'name2', 'age': 2, 'email': 'u2@a.com'},
+            {'Name': 'name3', 'age': 3, 'email': 'u3@a.com'}
+        ];
+
+        $scope.columns = [
+            {label: 'Name'},
+            {label: 'age'},
+            {label: 'email'}
+        ]
     }
 ]);
 
