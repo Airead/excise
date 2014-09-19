e51ba74eeb2943ea54cf81da4dd00d427b5f65cf
Airead Fan <fanrenhao@crazyhornets.com>
2014-09-19 18:36:08 +0800

    type finished

--- ml/bobgohome/bobgohome.js
+++ ml/bobgohome/bobgohome.js
@@ -30,11 +30,8 @@ function main() {
 
     document.body.appendChild(renderer.view);
 
-    var map = new BobsMap(stage);
-    gameInfo.map = map;
-    window.map = map;  // for debug
-
-    map.Render();
+    var gaBob = new GaBob();
+    GaBob.show(stage);
 
     registerKeyEvent();
 
@@ -86,7 +83,7 @@ function registerKeyEvent() {
 /*
  class BobsMap
 */
-function BobsMap(stage) {
+function BobsMap() {
     this.map = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
@@ -109,16 +106,11 @@ function BobsMap(stage) {
     this.endX = 0;
     this.endY = 2;
 
-    this.stage = stage;
-
     // init this.memory to 0
     this.memory = this.getCleanMemory();
 
     this.graphics = new PIXI.Graphics();
     this.memoryGraphics = new PIXI.Graphics();
-
-    this.stage.addChild(this.graphics);
-    this.stage.addChild(this.memoryGraphics);
 }
 
 BobsMap.prototype.drawRectWithColor = function (graphics, rectInfo, color) {
@@ -128,7 +120,7 @@ BobsMap.prototype.drawRectWithColor = function (graphics, rectInfo, color) {
     graphics.endFill();
 };
 
-BobsMap.prototype.Render = function () {
+BobsMap.prototype.render = function () {
     var i, j;
 
     var gh = this.graphics;
@@ -197,6 +189,16 @@ BobsMap.prototype.memoryRender = function () {
     }
 };
 
+BobsMap.prototype.show = function (stage) {
+    stage.addChild(this.graphics);
+    stage.addChild(this.memoryGraphics);
+};
+
+BobsMap.prototype.hide = function (stage) {
+    stage.removeChild(this.graphics);
+    stage.removeChild(this.memoryGraphics);
+};
+
 BobsMap.prototype.genTestPath = function () {
     var path = [];
     for (var i = 0; i < CHROMO_LENGTH; i++) {
@@ -258,3 +260,177 @@ function Genome(numBits) {
         this.bits.push(Math.floor(Math.random() * 2));
     }
 }
+
+// Genome end here
+
+/*
+ class GaBob
+*/
+function GaBob(crossRat, mutRat, popSize, numBits, geneLen) {
+    this.crossoverRate = crossRat;
+    this.mutationRate = mutRat;
+    this.popSize = popSize;
+    this.chromoLength = numBits;
+    this.totalFitnessScore = 0.0;
+    this.generation = 0;
+    this.geneLength = geneLen;
+    this.busy = false;
+
+    this.bobsBrain = null;
+    this.bobsMap = new BobsMap();
+
+    this.genomes = [];
+
+    this.createStartPopulation();
+}
+
+GaBob.prototype.createStartPopulation = function () {
+    this.genomes = [];
+    for (var i = 0; i < this.popSize; i++) {
+        this.genomes.push(new Genome(this.chromoLength));
+    }
+
+    this.generation = 0;
+    this.fittestGenome = 0;
+    this.bestFitnessScore = 0;
+    this.totalFitnessScore = 0;
+};
+
+GaBob.prototype.run = function () {
+    this.createStartPopulation();
+
+    this.busy = true;
+};
+
+GaBob.prototype.rouletteWheelSelection = function () {
+    var slice = Math.random() * this.totalFitnessScore;
+    var total = 0.0;
+    var selectedGenome = 0;
+
+    for (var i = 0; i < this.popSize; i++) {
+        total += this.genomes[i].fitness;
+        if (total > slice) {
+            selectedGenome = i;
+            break;
+        }
+    }
+
+    return this.genomes[selectedGenome];
+};
+
+GaBob.prototype.mutate = function (bits) {
+    for (var curBit = 0; curBit < bits.length; curBit++) {
+        if (Math.random() < this.mutationRate) {
+            if (bits[curBit] === 0) {
+                bits[curBit] = 1;
+            } else {
+                bits[curBit] = 0;
+            }
+        }
+    }
+};
+
+GaBob.prototype.crossover = function (mum, dad, baby1, baby2) {
+    var i;
+
+    if (Math.random() > this.crossoverRate || mum == dad) {
+        baby1.bits = mum.bits.slice(0);
+        baby2.bits = dad.bits.slice(0);
+        return;
+    }
+
+    // determine a crossover point
+    var cp = Math.floor(Math.random() * this.chromoLength);
+
+    baby1.bits = mum.bits.slice(0, cp).concat(dad.bits.slice(cp));
+    baby2.bits = data.bits.slice(0, cp).concat(mum.bits.slice(cp));
+};
+
+GaBob.prototype.epoch = function () {
+    this.updateFitnessScores();
+
+    // Now to create a new population
+    var newBabies = 0;
+    var babyGenomes;
+
+    while (newBabies < this.popSize) {
+        var mum = this.rouletteWheelSelection();
+        var dad = this.rouletteWheelSelection();
+
+        var baby1 = new Genome();
+        var baby2 = new Genome();
+
+        this.crossover(mum, data, baby1, baby2);
+
+        // operator - mutate
+        this.mutate(baby1.bits);
+        this.mutate(baby2.bits);
+
+        newBabies += 2;
+    }
+
+    this.genomes = babyGenomes;
+    this.generation++;
+};
+
+GaBob.prototype.updateFitnessScores = function () {
+    this.fittestGenome = 0;
+    this.bestFitnessScore = 0;
+    this.totalFitnessScore = 0;
+
+    var tmpMaps = new BobsMap();
+
+    for (var i = 0; i < this.popSize; i++) {
+        var gen = this.genomes[i];
+        var path = this.decode(gen.bits);
+        gen.fitness = this.bobsMap.TestRoute(path);
+
+        this.totalFitnessScore += gen.fitness;
+
+        if (gen.fitness > this.bestFitnessScore) {
+            this.bestFitnessScore = gen.fitness;
+            this.fittestGenome = i;
+            this.bobsBrain = tmpMaps;
+
+            if (gen.fitness === 1) {
+                this.busy = false;
+            }
+        }
+
+        tmpMaps.memory = tmpMaps.getCleanMemory();
+    }  // next genome
+};
+
+GaBob.prototype.decode = function (bits) {
+    var path = [];
+
+    var dirs = "nsew"; // North, South, East, West
+
+    for (var i = 0; i < bits.length; i+=this.geneLength) {
+        var gene = bits.slice(i, this.geneLength);
+        path.push(dirs[parseInt(gene, 2)]);
+    }
+    return path;
+};
+
+GaBob.prototype.render = function () {
+    var start;
+    this.bobsMap.render();
+    this.bobsBrain.memoryRender();
+
+    var s = "Generation: " + this.generation;
+
+    if (!this.busy) {
+        start = "Press Return to start a new run";
+        console.log(start);
+    } else {
+        start = "Space to stop";
+        console.log(start);
+    }
+};
+
+GaBob.prototype.show = function () {
+    this.bobsMap.show();
+};
+
+// GaBob end here
