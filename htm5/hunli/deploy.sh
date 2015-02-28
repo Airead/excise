a024e5dcb5d067e492932c4feedd9bddd760009c
Airead Fan <fanrenhao@crazyhornets.com>
2015-02-28 19:02:00 +0800

    hunli

---
+++ htm5/hunli/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -arv ../hunli atpackcn@atpackcn.com:www/airead/extra/
--- python/airclick/setup.py
+++ python/airclick/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='airclick',
     version='0.1',
-    py_modules=['yourscript'],
+    py_modules=['simpleClick'],
     install_requires=[
         'Click'
     ],
     entry_points='''
         [console_scripts]
-        simpleClick=simpeClick:cli
+        simpleClick=simpleClick:cli
     '''
 )
